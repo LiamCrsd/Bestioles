bb38cf52957b4ff145cfe302f1c887a5537800d7
yanice <yanice.moreau@imt-atlantique.net>
2022-12-02 17:34:45 +0100

    fixed wrong camoo factor

--- accessories/Accessory.cpp
+++ accessories/Accessory.cpp
@@ -2,4 +2,4 @@
 
 double Accessory::getSpeedFactor() {return 1;}
 double Accessory::getResistanceFactor() {return 1;}
-double Accessory::getCamouflageFactor() {return 1;}
+double Accessory::getCamouflageFactor() {return 0;}
