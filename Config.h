1166a3f20fb7167d25f0f9f90aedf040e92aba8b
LiamCrsd <liam.coursodon@gmail.com>
2022-12-14 22:28:29 +0100

    gestion d'un config.txt dans lequel sont enregistrer les données

--- Config.h
+++ Config.h
@@ -1,6 +1,8 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 #include <iostream>
+#include <fstream>
+#include <string>
 
 class Config {
     private:
@@ -40,41 +42,122 @@ class Config {
         }
 
         Config() {
-            width = 640;
-            height = 480;
-            minSize = 3;
-            maxSize = 15;
-            minSpeed = 1;
-            maxSpeed = 10;
-            ageMin = 100;
-            ageMax = 400;
-            repartition[0] = 0.2; //gregariousRate
-            repartition[1] = 0.2; //fearfulRate
-            repartition[2] = 0.2; //kamikazeRate
-            repartition[3] = 0.2; //farsightedRate
-            repartition[4] = 0.2; //multiplePersonnalityRate
-            birthRate = 0.01;
-            deathRateMax = 0.5;
-            cloneRateMax = 0.01;
-            fieldViewMin = M_PI/8;
-            fieldViewMax = M_PI/2;
-            detectionRateMin = 0.3;
-            detectionRateMax = 1;
-            detectionDistanceMin = 20;
-            detectionDistanceMax = 100;
-            eyesRate = 0.5;
-            earsRate = 0.5;
-            speedFactorMin = 1.2;
-            speedFactorMax = 2;
-            resistanceFactorMin = 1.5;
-            resistanceFactorMax = 3;
-            slowFactorMin = 3;
-            slowFactorMax = 8;
-            camouflageFactorMin = 0.6;
-            camouflageFactorMax = 1,
-            scaredSpeedMax = 5;
-            scaredThresholdMax = 5;
-            switchRateMax = 0.1;
+            ifstream fichier("./config.txt");
+            if(fichier)
+            {
+              string ligne;
+              getline(fichier,ligne);
+              width = stoi(ligne);
+              getline(fichier,ligne);
+              height = stoi(ligne);
+              getline(fichier,ligne);
+              minSize = stod(ligne);
+              cout << minSize << endl;
+              getline(fichier,ligne);
+              maxSize = stod(ligne);
+              getline(fichier,ligne);
+              minSpeed = stod(ligne);
+              getline(fichier,ligne);
+              maxSpeed = stod(ligne);
+              getline(fichier,ligne);
+              ageMin = stoi(ligne);
+              getline(fichier,ligne);
+              ageMax = stoi(ligne);
+              getline(fichier,ligne);
+              repartition[0] = stod(ligne); //gregariousRate
+              getline(fichier,ligne);
+              repartition[1] = stod(ligne); //fearfulRate
+              getline(fichier,ligne);
+              repartition[2] = stod(ligne); //kamikazeRate
+              getline(fichier,ligne);
+              repartition[3] = stod(ligne); //farsightedRate
+              getline(fichier,ligne);
+              repartition[4] = stod(ligne); //multiplePersonnalityRate
+              getline(fichier,ligne);
+              birthRate = stod(ligne);
+              getline(fichier,ligne);
+              deathRateMax = stod(ligne);
+              getline(fichier,ligne);
+              cloneRateMax = stod(ligne);
+              getline(fichier,ligne);
+              fieldViewMin = M_PI/stod(ligne);
+              getline(fichier,ligne);
+              fieldViewMax = M_PI/stod(ligne);
+              getline(fichier,ligne);
+              detectionRateMin = stod(ligne);
+              getline(fichier,ligne);
+              detectionRateMax = stod(ligne);
+              getline(fichier,ligne);
+              detectionDistanceMin = stod(ligne);
+              getline(fichier,ligne);
+              detectionDistanceMax = stod(ligne);
+              getline(fichier,ligne);
+              eyesRate = stod(ligne);
+              getline(fichier,ligne);
+              earsRate = stod(ligne);
+              getline(fichier,ligne);
+              speedFactorMin = stod(ligne);
+              getline(fichier,ligne);
+              speedFactorMax = stod(ligne);
+              getline(fichier,ligne);
+              resistanceFactorMin = stod(ligne);
+              getline(fichier,ligne);
+              resistanceFactorMax = stod(ligne);
+              getline(fichier,ligne);
+              slowFactorMin = stod(ligne);
+              getline(fichier,ligne);
+              slowFactorMax = stod(ligne);
+              getline(fichier,ligne);
+              camouflageFactorMin = stod(ligne);
+              getline(fichier,ligne);
+              camouflageFactorMax = stod(ligne);
+              getline(fichier,ligne);
+              scaredSpeedMax = stod(ligne);
+              getline(fichier,ligne);
+              scaredThresholdMax = stod(ligne);
+              getline(fichier,ligne);
+              switchRateMax = stod(ligne);
+            }
+            else
+            {
+              cout << "error txt" << endl;
+              width = 640;
+              height = 480;
+              minSize = 3;
+              maxSize = 15;
+              minSpeed = 1;
+              maxSpeed = 10;
+              ageMin = 100;
+              ageMax = 400;
+              repartition[0] = 0.2; //gregariousRate
+              repartition[1] = 0.2; //fearfulRate
+              repartition[2] = 0.2; //kamikazeRate
+              repartition[3] = 0.2; //farsightedRate
+              repartition[4] = 0.2; //multiplePersonnalityRate
+              birthRate = 0.01;
+              deathRateMax = 0.5;
+              cloneRateMax = 0.01;
+              fieldViewMin = M_PI/8;
+              fieldViewMax = M_PI/2;
+              detectionRateMin = 0.3;
+              detectionRateMax = 1;
+              detectionDistanceMin = 20;
+              detectionDistanceMax = 100;
+              eyesRate = 0.5;
+              earsRate = 0.5;
+              speedFactorMin = 1.2;
+              speedFactorMax = 2;
+              resistanceFactorMin = 1.5;
+              resistanceFactorMax = 3;
+              slowFactorMin = 3;
+              slowFactorMax = 8;
+              camouflageFactorMin = 0.6;
+              camouflageFactorMax = 1,
+              scaredSpeedMax = 5;
+              scaredThresholdMax = 5;
+              switchRateMax = 0.1;
+            }
+
         }
 
     public:
