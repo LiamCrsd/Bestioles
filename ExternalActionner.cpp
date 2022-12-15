d43f05391d59efffb07d74f9f263a7242621296a
l21scist <luca.scistri@imt-atlantique.net>
2022-12-15 01:40:54 +0100

    added ability to kill a specific bestiole

--- ExternalActionner.cpp
+++ ExternalActionner.cpp
@@ -39,11 +39,12 @@ void ExternalActionner::redirectKey(int key) {
     if (key == 98) this->killBestiole();
     if (key == 99) this->getReport();
     if (key == 100) this->createBestiole(0);
-    if (key == 101) this->changeBehavior(0, 1);
-    if (key == 102) this->changeBehavior(0, 2);
-    if (key == 103) this->changeBehavior(0, 3);
-    if (key == 104) this->changeBehavior(0, 4);
-    if (key == 105) this->changeBehavior(0, 0);
+    if (key == 101) this->changeBehavior(0, 0);
+    if (key == 102) this->changeBehavior(0, 1);
+    if (key == 103) this->changeBehavior(0, 2);
+    if (key == 104) this->changeBehavior(0, 3);
+    if (key == 105) this->changeBehavior(0, 4);
+    if (key == 106) this->killBestiole(0);
 }
 
 void ExternalActionner::killBestiole(){
@@ -52,6 +53,11 @@ void ExternalActionner::killBestiole(){
         auto ptr_bestiole = collectionBestiole->getBestiolesList()[index];
         ptr_bestiole->setDead(true);
     }
+}
+
+void ExternalActionner::killBestiole(int index){
+    auto listeBestiole = collectionBestiole->getBestiolesList();
+    collectionBestiole->delBestiole(listeBestiole.at(index));
 }
 
 void ExternalActionner::getReport(){
